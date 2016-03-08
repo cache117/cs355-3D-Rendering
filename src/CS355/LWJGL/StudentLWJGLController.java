293950b19c245deab361968dad7edaf3d0f11959
Cache Staheli <cache@byu.edu>
2016-03-07 23:19:48 -0700

    Neighborhood draws correctly. Cheat-codes enabled. Finished.

--- src/CS355/LWJGL/StudentLWJGLController.java
+++ src/CS355/LWJGL/StudentLWJGLController.java
@@ -1,12 +1,14 @@
 package CS355.LWJGL;
 
 import org.lwjgl.input.Keyboard;
+import sun.rmi.runtime.Log;
 
 import java.util.Iterator;
 import java.util.logging.Logger;
 
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.util.glu.GLU.gluPerspective;
+import static org.lwjgl.input.Keyboard.isKeyDown;
 
 /**
  * @author Cache Staheli
@@ -14,21 +16,18 @@ import static org.lwjgl.util.glu.GLU.gluPerspective;
 public class StudentLWJGLController implements CS355LWJGLController
 {
     private static final Logger LOGGER = Logger.getLogger(StudentLWJGLController.class.getName());
-    private static final float ASPECT = (LWJGLSandbox.DISPLAY_WIDTH / LWJGLSandbox.DISPLAY_HEIGHT);
-    private static final float CLIPPING = ASPECT / 2;
-    private static final float ROTATION = 1.5f, UNIT = 1.0f;
-    private static final float FOVY = 65f;
-    private static final float Z_NEAR = 1, Z_FAR = 1000;
 
-    //This is a model of a house.
-    //It has a single method that returns an iterator full of Line3Ds.
-    //A "Line3D" is a wrapper class around two Point3Ds.
-    //It should all be fairly intuitive if you look at those classes.
-    //If not, I apologize.
+    private static final float ASPECT = (LWJGLSandbox.DISPLAY_WIDTH / LWJGLSandbox.DISPLAY_HEIGHT);
+    private static final float UNIT = 1.0f;
+    private static final float FOVY = 65f;
+    private static final float CLIPPING = FOVY / 1.3f;
+    private static final float Z_NEAR = 1, Z_FAR = 200;
+
     private final WireFrame model;
 
     private Point3D cameraLocation;
     private double rotation;
+    private float movementAmount;
 
     /**
      * Initializes the controller.
@@ -47,14 +46,20 @@ public class StudentLWJGLController implements CS355LWJGLController
     @Override
     public void resizeGL()
     {
-        cameraLocation = new Point3D(0, 0, 0);
-        rotation = 0.0;
+        cameraLocation = new Point3D(-20, -2, 0);
+        rotation = 270.0f;
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
         glViewport(0, 0, LWJGLSandbox.DISPLAY_WIDTH, LWJGLSandbox.DISPLAY_HEIGHT);
         loadPerspective();
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        movementAmount = UNIT;
     }
 
+    /**
+     * Not currently used.
+     */
     @Override
     public void update()
     {
@@ -71,119 +76,150 @@ public class StudentLWJGLController implements CS355LWJGLController
     public void updateKeyboard()
     {
         //a move left
-        if (Keyboard.isKeyDown(Keyboard.KEY_A))
+        if (isKeyDown(Keyboard.KEY_A))
         {
             moveLeft();
         }
         //d move right
-        else if (Keyboard.isKeyDown(Keyboard.KEY_D))
+        else if (isKeyDown(Keyboard.KEY_D))
         {
             moveRight();
         }
         //w move forward
-        else if (Keyboard.isKeyDown(Keyboard.KEY_W))
+        else if (isKeyDown(Keyboard.KEY_W))
         {
             moveForward();
         }
         //s move backward
-        else if (Keyboard.isKeyDown(Keyboard.KEY_S))
+        else if (isKeyDown(Keyboard.KEY_S))
         {
             moveBackward();
         }
         //q turn left
-        else if (Keyboard.isKeyDown(Keyboard.KEY_Q))
+        else if (isKeyDown(Keyboard.KEY_Q))
         {
             rotateLeft();
         }
         //e turn right
-        else if (Keyboard.isKeyDown(Keyboard.KEY_E))
+        else if (isKeyDown(Keyboard.KEY_E))
         {
             rotateRight();
         }
         //r move up
-        else if (Keyboard.isKeyDown(Keyboard.KEY_R))
+        else if (isKeyDown(Keyboard.KEY_R))
         {
             moveUp();
         }
         //f move down
-        else if (Keyboard.isKeyDown(Keyboard.KEY_F))
+        else if (isKeyDown(Keyboard.KEY_F))
         {
             moveDown();
         }
         //h return to original "home" position and orientation
-        else if (Keyboard.isKeyDown(Keyboard.KEY_H))
+        else if (isKeyDown(Keyboard.KEY_H))
         {
-            LOGGER.info("Returning to Home");
+            LOGGER.fine("Returning to Home");
             resizeGL();
-            //Do processing
         }
         //o switch to orthographic projection
-        else if (Keyboard.isKeyDown(Keyboard.KEY_O))
+        else if (isKeyDown(Keyboard.KEY_O))
         {
             glMatrixMode(GL_PROJECTION);
             glLoadIdentity();
             glOrtho(-CLIPPING, CLIPPING, -CLIPPING, CLIPPING, Z_NEAR, Z_FAR);
         }
         //p switch to perspective projection
-        else if (Keyboard.isKeyDown(Keyboard.KEY_P))
+        else if (isKeyDown(Keyboard.KEY_P))
         {
             glMatrixMode(GL_PROJECTION);
             glLoadIdentity();
             loadPerspective();
+        } else
+        {
+            cheatCodes();
         }
         buildModelViewMatrix();
     }
 
+    private void cheatCodes()
+    {
+        if (isKeyDown(Keyboard.KEY_1))
+        {
+            movementAmount = UNIT * 1.0f;
+            LOGGER.info("Normal speed");
+        }
+        else if (isKeyDown(Keyboard.KEY_2))
+        {
+            movementAmount = UNIT * 2.0f;
+            LOGGER.info("Quicker speed");
+        }
+        else if (isKeyDown(Keyboard.KEY_3))
+        {
+            movementAmount = UNIT * 3.0f;
+            LOGGER.info("Fast speed");
+        }
+        else if (isKeyDown(Keyboard.KEY_4))
+        {
+            movementAmount = UNIT * 4.0f;
+            LOGGER.info("Really Fast speed");
+        }
+        else if (isKeyDown(Keyboard.KEY_5))
+        {
+            movementAmount = UNIT * 10.0f;
+            LOGGER.info("WARNING: Super-sonic speed");
+        }
+    }
+
     private void moveForward()
     {
-        LOGGER.info("Moving Forward");
-        cameraLocation.x -= UNIT * (float) Math.sin(Math.toRadians(rotation));
-        cameraLocation.z += UNIT * (float) Math.cos(Math.toRadians(rotation));
+        LOGGER.fine("Moving Forward");
+        cameraLocation.x -= movementAmount * sin(rotation);
+        cameraLocation.z += movementAmount * cos(rotation);
     }
 
     private void moveBackward()
     {
-        LOGGER.info("Moving Backward");
-        cameraLocation.x += UNIT * (float) Math.sin(Math.toRadians(rotation));
-        cameraLocation.z -= UNIT * (float) Math.cos(Math.toRadians(rotation));
+        LOGGER.fine("Moving Backward");
+        cameraLocation.x += movementAmount * sin(rotation);
+        cameraLocation.z -= movementAmount * cos(rotation);
     }
 
     private void moveLeft()
     {
-        LOGGER.info("Moving Left");
-        cameraLocation.x += UNIT * (float) Math.cos(Math.toRadians(rotation));
-        cameraLocation.z -= UNIT * (float) Math.sin(Math.toRadians(rotation));
+        LOGGER.fine("Moving Left");
+        cameraLocation.x += movementAmount * cos(rotation);
+        cameraLocation.z += movementAmount * sin(rotation);
     }
 
     private void moveRight()
     {
-        LOGGER.info("Moving Right");
-        cameraLocation.x -= UNIT * (float) Math.cos(Math.toRadians(rotation));
-        cameraLocation.z += UNIT * (float) Math.sin(Math.toRadians(rotation));
+        LOGGER.fine("Moving Right");
+        cameraLocation.x -= movementAmount * cos(rotation);
+        cameraLocation.z -= movementAmount * sin(rotation);
     }
 
     private void moveUp()
     {
-        LOGGER.info("Moving Up");
-        cameraLocation.y += UNIT;
+        LOGGER.fine("Moving Up");
+        cameraLocation.y += movementAmount;
     }
 
     private void moveDown()
     {
-        LOGGER.info("Moving Down");
-        cameraLocation.y -= UNIT;
+        LOGGER.fine("Moving Down");
+        cameraLocation.y -= movementAmount;
     }
 
     private void rotateRight()
     {
-        LOGGER.info("Rotating Right");
-        rotation += ROTATION;
+        LOGGER.fine("Rotating Right");
+        rotation += (movementAmount * 1.5f);
     }
 
     private void rotateLeft()
     {
-        LOGGER.info("Rotating Left");
-        rotation -= ROTATION;
+        LOGGER.fine("Rotating Left");
+        rotation -= (movementAmount * 1.5f);
     }
 
     private void loadPerspective()
@@ -197,6 +233,7 @@ public class StudentLWJGLController implements CS355LWJGLController
         glLoadIdentity();
         rotate((float) rotation);
         translateByPoint(cameraLocation);
+        LOGGER.fine(cameraLocation.toString());
     }
 
     private void translateByPoint(Point3D point)
@@ -227,6 +264,7 @@ public class StudentLWJGLController implements CS355LWJGLController
             for (float j = 0.0f; j <= 1.0f; j += 1.0f)
             {
                 glPushMatrix();
+                glMatrixMode(GL_MODELVIEW);
                 useRandomColor(i, j);
                 glTranslatef(j * houseSeparation, 0, i * -houseSeparation);
                 float rotation = (j == 1) ? 270.0f : 90.0f;
@@ -263,9 +301,14 @@ public class StudentLWJGLController implements CS355LWJGLController
         glColor3f(red, green, blue);
     }
 
-    private double calculateRadians(double degrees)
+    private float cos(double rotation)
     {
-        return Math.toRadians(degrees);
+        return (float) Math.cos(Math.toRadians(rotation));
+    }
+
+    private float sin(double rotation)
+    {
+        return (float) Math.sin(Math.toRadians(rotation));
     }
 
 }
